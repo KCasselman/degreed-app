c57b43f84ce49465ea395e3d7a401a427babed00
Kristen Casselman <kecasselman@herffjones.com>
2022-11-18 17:02:17 -0500

    using storybook button for imdb button

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   type="button"
   (click)="onClick.emit($event)"
   [ngClass]="classes"
-  [ngStyle]="{ 'background-color': backgroundColor }"
+  [ngStyle]="{ 'background-color': backgroundColor, 'border-radius': borderRadius, 'position': position, 'bottom': bottom, 'right': right, 'margin': margin, 'padding': padding, 'border': border, 'color': color, 'font-weight': fontWeight, 'font-family': fontFamily, 'font-size': fontSize }"
 >
   {{ label }}
 </button>`,
@@ -44,6 +44,66 @@ export default class ButtonComponent {
    */
   @Input()
   label = 'Button';
+
+  /**
+   * What position value to use
+   */
+  @Input()
+  position?: string;
+
+  /**
+   * What bottom value to use
+   */
+  @Input()
+  bottom?: string;
+
+  /**
+   * What right value to use
+   */
+  @Input()
+  right?: string;
+
+  /**
+   * What margin to use
+   */
+   @Input()
+   margin?: string;
+
+  /**
+   * What padding to use
+   */
+   @Input()
+   padding?: string;
+
+  /**
+   * What border to use
+   */
+   @Input()
+   border?: string;
+
+  /**
+   * What color to use
+   */
+   @Input()
+   color?: string;
+
+  /**
+   * What font weight to use
+   */
+   @Input()
+   fontWeight?: string;
+
+  /**
+   * What font family to use
+   */
+   @Input()
+   fontFamily?: string;
+
+  /**
+   * What font size to use
+   */
+   @Input()
+   fontSize?: string;
 
   /**
    * Optional click handler
